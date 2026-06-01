eb1589646bd72250d75da97d8ba1c19bf46540f6
ferjosegonza <96452620+ferjosegonza@users.noreply.github.com>
2026-06-01 14:48:12 -0300

    agregué url de la github page al array de dominios permitidos en codigo.gs

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    GAS_URL: "https://script.google.com/macros/s/AKfycbxPNjP_w2HlLewIzagwJYZn8snWo0SlZvnq5yVbkFoHU6cL3TCnsmF9CnkqoIzsvHlgTg/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycbz7R3PaslwQXcXUzYKw_1i4hha-GIZxRwz2DLsZ7qeVsubHPu7TPwi0_ijIuXLOcNjQmg/exec",
     SESSION_EXPIRATION: 24 * 60 * 60 * 1000
 };
