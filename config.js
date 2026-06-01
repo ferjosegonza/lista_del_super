0f635bac04bba639a988388084ebf2a90d558a8b
ferjosegonza <96452620+ferjosegonza@users.noreply.github.com>
2026-06-01 15:00:46 -0300

    cambiar funcion de codigo.gs pa admitir celu

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    GAS_URL: "https://script.google.com/macros/s/AKfycbz7R3PaslwQXcXUzYKw_1i4hha-GIZxRwz2DLsZ7qeVsubHPu7TPwi0_ijIuXLOcNjQmg/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycbwSXSjz59A1hOmOb13Q6X1u4M9I0jGqq06H7eDRAuxHAAmzHD9uLnmlgdYzdophj-6SSw/exec",
     SESSION_EXPIRATION: 24 * 60 * 60 * 1000
 };
