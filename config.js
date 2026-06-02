2242a754208a1270a1a0ac97b7d46e0bb53c6c2d
ferjosegonza <96452620+ferjosegonza@users.noreply.github.com>
2026-06-02 16:11:39 -0300

    sdf

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    GAS_URL: "https://script.google.com/macros/s/AKfycbwAEjrunNURGujo8Ojx_lKBtIA1mSNe8j-Z8gA5TW8MMNfev9cg72baMDSZmZySrvYUbA/exec",
+    GAS_URL: "https://script.google.com/macros/s/AKfycbydCbDIQqS72XbL23d9kDZI7GLUKstNFHtBeXMEiSrXwewfGL0CgSRLeAQVq4snO4Egtw/exec",
     SESSION_EXPIRATION: 24 * 60 * 60 * 1000
 };
