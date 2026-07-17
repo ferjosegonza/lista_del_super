86078b7f524ba1d22e65df4668b6b91c123dbf3a
Fernando Jose Gonzalez <fer.jose.gonza@gmail.com>
2026-07-17 12:55:07 -0300

    front 100%

--- config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    GAS_URL: "https://script.google.com/macros/s/AKfycbyxt24cTAuODa_AIBVUlPQhi5yjk8FU1RSD7A0sqeYrx4j5hX6UQHgk_16p3Rw0dkJcCA/exec",
-    SESSION_EXPIRATION: 24 * 60 * 60 * 1000
-};
